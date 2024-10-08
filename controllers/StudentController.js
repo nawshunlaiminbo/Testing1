8468c1d6cf0b38973f1469fdd86dc0e1f129d0c9
Hsu Aung <hsuaung633@gmail.com>
2024-10-08 20:04:45 +0630

    seeding and update,delete

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,38 +1,74 @@
-const studentdata = require('../models').Student;
-const{Sequelize} = require('sequelize');
+const studentdata = require("../models").Student;
+const { Sequelize } = require("sequelize");
 
-const getAllStudents = async(req,res)=>{
-    await studentdata.findAll()
-    .then(students=>{
-        if(students.length>0){
-            res.status(200).json(students);
-        }
-        else{
-            res.status(404).json("No Student Data.");
-        }
+const getAllStudents = async (req, res) => {
+  await studentdata
+    .findAll()
+    .then((students) => {
+      if (students.length > 0) {
+        res.status(200).json(students);
+      } else {
+        res.status(404).json("No Student Data.");
+      }
     })
-    .catch(error=>{
-        res.status(500).json("Error"+error.message);
-    })
-}
-const addStudent = async(req,res)=>{
-    await studentdata.findOne({where:{name:req.body.name}})
-   .then(student=>{
-    if(student != null){
-        return res.status(400).json("Student already exist.")
-    }
-    else{
+    .catch((error) => {
+      res.status(500).json("Error" + error.message);
+    });
+};
+const addStudent = async (req, res) => {
+  await studentdata
+    .findOne({ where: { name: req.body.name } })
+    .then((student) => {
+      if (student != null) {
+        return res.status(400).json("Student already exist.");
+      } else {
         return studentdata.create(req.body);
-    }
-   })
-   .then(()=>{
-    res.status(201).json("Student created Successfully.");
-   })
-   .catch(err=>{
-        return res.status(400).json({error: errorMessages});
-})
-}
+      }
+    })
+    .then(() => {
+      res.status(201).json("Student created Successfully.");
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: errorMessages });
+    });
+};
+
+const editStudent = async (req, res) => {
+  await studentdata
+    .findOne({ where: { id: req.params.id } })
+    .then((student) => {
+      if (student != null) {
+        student.update(req.body, { where: { id: req.params.id } }).then((_) => {
+          res.status(200).json("student updated");
+        });
+      } else {
+        res.status(404).json("student not found");
+      }
+    })
+    .catch((err) => {
+      res.status(500).json("Error : " + err);
+    });
+};
+
+const deleteStudent = async (req, res) => {
+  await studentdata
+    .findByPk(req.params.id)
+    .then((student) => {
+      if (student != null) {
+        student.destroy({ where: { id: req.params.id } }).then((_) => {
+          res.status(200).json("student deleted");
+        });
+      } else {
+        res.status(404).json("student not found");
+      }
+    })
+    .catch((err) => {
+      res.status(500).json("Error : " + err);
+    });
+};
 module.exports = {
-    getAllStudents,
-    addStudent
-}
+  getAllStudents,
+  addStudent,
+  editStudent,
+  deleteStudent,
+};
--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const studentController= require('../controllers/StudentController');
+const studentController = require("../controllers/StudentController");
 
-router.get('/list',studentController.getAllStudents);
-router.post('/add',studentController.addStudent);
+router.get("/list", studentController.getAllStudents);
+router.post("/add", studentController.addStudent);
+router.put("/edit/:id", studentController.editStudent);
+router.delete("/delete/:id", studentController.deleteStudent);
 
 module.exports = router;
